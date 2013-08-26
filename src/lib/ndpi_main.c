51f0d841e30ddd9e5820adb1ef8f573808a0744c
Luca Deri <deri@ntop.org>
2013-08-26 21:22:52 +0000

    Minor fix
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/nDPI@6712 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -5178,7 +5178,7 @@ unsigned int ndpi_guess_undetected_protocol(struct ndpi_detection_module_struct 
 /* ****************************************************** */
 
 char* ndpi_get_proto_name(struct ndpi_detection_module_struct *ndpi_mod, u_int16_t proto_id) {
-  if(proto_id > ndpi_mod->ndpi_num_supported_protocols) proto_id = NDPI_PROTOCOL_UNKNOWN;
+  if(proto_id >= ndpi_mod->ndpi_num_supported_protocols) proto_id = NDPI_PROTOCOL_UNKNOWN;
   return(ndpi_mod->proto_defaults[proto_id].protoName);
 }
 
