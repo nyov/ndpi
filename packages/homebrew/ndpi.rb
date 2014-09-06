0a7dad93a62dff4257dc9864732880308778cb2f
Luca Deri <deri@ntop.org>
2014-09-06 13:22:19 +0000

    Updated homebew formula
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/nDPI@8204 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- packages/homebrew/ndpi.rb
+++ packages/homebrew/ndpi.rb
@@ -2,23 +2,22 @@ require "formula"
 
 class Ndpi < Formula
   homepage "http://www.ntop.org/products/ndpi/"
-  url "https://downloads.sourceforge.net/project/ntop/nDPI/libndpi-1.5.0_r8115.tar.gz"
-  sha1 "9fbb97dd3de35670c8f527ac3ccdb1aa50a4da70"
-  version "1.5.0"
+  url "https://downloads.sourceforge.net/project/ntop/nDPI/libndpi-1.5.1.tar.gz"
+  sha1 "0a6ed585545ab6611f3f0ac9efd9eb36bb5481dd"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "libtool" => :build
+  depends_on "json-c"
 
   def install
     system "./configure","--prefix=#{prefix}"
     system "make"
-    system "make","install"
+    system "make", "install"
   end
 
   test do
     system "#{bin}/ndpiReader","-h"
   end
-
 end
