123b549c6d4fcad8f27f869e15f62f79b04fac5b
Tomasz Bujlow <tomasz@skatnet.dk>
2014-11-01 13:39:24 +0000

    The example app is now able to handle normal PCAPs having multiple flows inside
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/nDPIng@8526 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- example/example_ndping.c
+++ example/example_ndping.c
@@ -34,27 +34,396 @@
 #include <dirent.h>
 #include "ndpi_main.h"
 
-struct ndpi_detection_module_struct *ndpi_detection_module_struct_pointer = NULL;
-struct ndpi_flow_struct *ndpi_flow_struct_pointer = NULL;
+/* ************************************************************************************************************************************************ */
+/* STUFF ASSOCIATED WITH THE TREE OF GENERATED RESULTS */
+/* ************************************************************************************************************************************************ */
 
-int process_packet_by_ndpi(u_int64_t time, bpf_u_int32 header_len, u_char *packet) {
+#define NUM_ROOTS        512
 
-	const struct ndpi_ethhdr *ethernet = (struct ndpi_ethhdr *) packet;
-	u_int16_t ip_offset = sizeof(struct ndpi_ethhdr);
+typedef struct ndpi_flow {
+	u_int32_t lower_ip;
+	u_int32_t upper_ip;
+	u_int16_t lower_port;
+	u_int16_t upper_port;
+	u_int8_t detection_completed, protocol;
+	struct ndpi_flow_struct *ndpi_flow_struct_pointer;
+	char lower_name[32], upper_name[32];
 
-	if (ntohs(ethernet->h_proto) == 0x8100 /* VLAN */) {
-		ip_offset += 4;
+	u_int16_t processed_packets;
+
+} ndpi_flow_t;
+
+static struct ndpi_flow *ndpi_flows_root[NUM_ROOTS] = { NULL };
+
+typedef struct node_t {
+	char *key;
+	struct node_t *left, *right;
+} ndpi_node;
+
+typedef enum {
+	ndpi_preorder,
+	ndpi_postorder,
+	ndpi_endorder,
+	ndpi_leaf
+} ndpi_VISIT;
+
+static void ndpi_tdestroy_recurse(ndpi_node* root, void (*free_action)(void *)) {
+  
+	if (root->left != NULL) {
+		ndpi_tdestroy_recurse(root->left, free_action);
+	}
+	
+	if (root->right != NULL){
+		ndpi_tdestroy_recurse(root->right, free_action);
 	}
 
-	struct ndpi_iphdr *iph = (struct ndpi_iphdr *) &packet[ip_offset];
-	u_int16_t ipsize = header_len - ip_offset;
+	(*free_action) ((void *) root->key);
+	free(root);
+}
 
-	return ndpi_process_ip_packet(ndpi_detection_module_struct_pointer, ndpi_flow_struct_pointer, (uint8_t *) iph, ipsize, time);
+void ndpi_tdestroy(void *vrootp, void (*freefct)(void *)) {
+  
+	ndpi_node *root = (ndpi_node *) vrootp;
+
+	if (root != NULL) {
+		ndpi_tdestroy_recurse(root, freefct);
+	}
+}
+
+void *ndpi_tsearch(const void *vkey, void **vrootp, int (*compar)(const void *, const void *)) {
+  
+	ndpi_node *q;
+	char *key = (char *) vkey;
+	ndpi_node **rootp = (ndpi_node **) vrootp;
+
+	if (rootp == (ndpi_node **) 0) {
+		return ((void *) 0);
+	}
+
+	while (*rootp != (ndpi_node *) 0) {	/* Knuth's T1: */
+		int r;
+
+		if ((r = (*compar)(key, (*rootp)->key)) == 0)	/* T2: */ {
+			return ((void *)*rootp);		/* we found it! */
+		}
+		
+		rootp = (r < 0) ?
+		    &(*rootp)->left :		/* T3: follow left branch */
+		    &(*rootp)->right;		/* T4: follow right branch */
+	}
+
+	q = (ndpi_node *) malloc(sizeof(ndpi_node));	/* T5: key not found */
+
+	if (q != (ndpi_node *) 0) {	/* make new node */
+		*rootp = q;			/* link new node to old */
+		q->key = key;			/* initialize new node */
+		q->left = q->right = (ndpi_node *)0;
+	}
+
+	return ((void *) q);
+}
+
+void *ndpi_tfind(const void *vkey, void *vrootp, int (*compar)(const void *, const void *)) {
+ 
+	char *key = (char *)vkey;
+	ndpi_node **rootp = (ndpi_node **)vrootp;
+
+	if (rootp == (ndpi_node **) 0) {
+		return ((ndpi_node *) 0);
+	}
+
+	while (*rootp != (ndpi_node *) 0) {	/* T1: */
+		  
+		int r;
+		
+		if ((r = (*compar) (key, (*rootp)->key)) == 0)	/* T2: */ {
+			return (*rootp);		/* key found */
+		}
+		
+		rootp = (r < 0) ?
+		    &(*rootp)->left :		/* T3: follow left branch */
+		    &(*rootp)->right;		/* T4: follow right branch */
+	}
+	
+	return (ndpi_node *) 0;
+}
+
+static void ndpi_trecurse(ndpi_node *root, void (*action)(const void *, ndpi_VISIT, int, void*), int level, void *user_data) {
+
+	if (root->left == (ndpi_node *) 0 && root->right == (ndpi_node *) 0) {
+		(*action) (root, ndpi_leaf, level, user_data);
+	} else {
+		  
+		(*action) (root, ndpi_preorder, level, user_data);
+		
+		if (root->left != (ndpi_node *) 0) {
+			ndpi_trecurse(root->left, action, level + 1, user_data);
+		}
+		
+		(*action) (root, ndpi_postorder, level, user_data);
+		
+		if (root->right != (ndpi_node *) 0) {
+			ndpi_trecurse(root->right, action, level + 1, user_data);
+		}
+		
+		(*action) (root, ndpi_endorder, level, user_data);
+	}
+}
+
+void ndpi_twalk(const void *vroot, void (*action)(const void *, ndpi_VISIT, int, void *), void *user_data) {
+  
+	ndpi_node *root = (ndpi_node *) vroot;
+
+	if (root != (ndpi_node *) 0 && action != (void (*)(const void *, ndpi_VISIT, int, void*)) 0) {
+		ndpi_trecurse(root, action, 0, user_data);
+	}
+}
+
+/* ************************************************************************************************************************************************ */
+/* MAIN APPLICATION */
+/* ************************************************************************************************************************************************ */
+
+#define	MAX_NDPI_FLOWS	2000000
+
+static u_int32_t ndpi_flow_count = 0;
+struct ndpi_detection_module_struct *ndpi_detection_module_struct_pointer = NULL;
+
+
+static int node_cmp(const void *a, const void *b) {
+
+	struct ndpi_flow *fa = (struct ndpi_flow*) a;
+	struct ndpi_flow *fb = (struct ndpi_flow*) b;
+
+	if (fa->lower_ip < fb->lower_ip) {
+		return -1;
+	} else if (fa->lower_ip > fb->lower_ip) {
+		return 1;
+	}
+
+	if (fa->lower_port < fb->lower_port) {
+		return -1;
+	} else if (fa->lower_port > fb->lower_port) {
+		return 1;
+	}
+
+	if (fa->upper_ip < fb->upper_ip) {
+		return -1;
+	} else if (fa->upper_ip > fb->upper_ip) {
+		return 1;
+	}
+
+	if (fa->upper_port < fb->upper_port) {
+		return -1;
+	} else if (fa->upper_port > fb->upper_port) {
+		return 1;
+	}
+
+	if (fa->protocol < fb->protocol) {
+		return -1;
+	} else if (fa->protocol > fb->protocol) {
+		return 1;
+	}
+
+	return 0;
+}
+
+static void free_ndpi_flow(struct ndpi_flow *flow) {
+	if (flow->ndpi_flow_struct_pointer) {
+		free(flow->ndpi_flow_struct_pointer);
+		flow->ndpi_flow_struct_pointer = NULL;
+	}
+}
+
+static void ndpi_flow_freer(void *node) {
+	struct ndpi_flow *flow = (struct ndpi_flow*) node;
+	free_ndpi_flow(flow);
+	free(flow);
+}
+
+static struct ndpi_flow *get_ndpi_flow(const struct ndpi_iphdr *iph, u_int16_t ipsize, u_int16_t l4_packet_len) {
+	  
+	u_int32_t idx, l4_offset;
+	struct ndpi_tcphdr *tcph = NULL;
+	struct ndpi_udphdr *udph = NULL;
+	u_int32_t lower_ip;
+	u_int32_t upper_ip;
+	u_int16_t lower_port;
+	u_int16_t upper_port;
+	struct ndpi_flow flow;
+	void *ret;
+
+	if (ipsize < 20) {
+		return NULL;
+	}
+
+	if ((iph->ihl * 4) > ipsize || ipsize < ntohs(iph->tot_len) || (iph->frag_off & htons(0x1FFF)) != 0) {
+		return NULL;
+	}
+
+	if (iph->saddr < iph->daddr) {
+		lower_ip = iph->saddr;
+		upper_ip = iph->daddr;
+	} else {
+		lower_ip = iph->daddr;
+		upper_ip = iph->saddr;
+	}
+
+	l4_offset = iph->ihl * 4;
+
+	if (iph->protocol == 6 && l4_packet_len >= 20) {
+		// tcp
+		tcph = (struct ndpi_tcphdr *) ((u_int8_t *) iph + l4_offset);
+
+		if (iph->saddr < iph->daddr) {
+			lower_port = tcph->source;
+			upper_port = tcph->dest;
+		} else {
+			  lower_port = tcph->dest;
+			upper_port = tcph->source;
+		}
+	} else if (iph->protocol == 17 && l4_packet_len >= 8) {
+		// udp
+		udph = (struct ndpi_udphdr *) ((u_int8_t *) iph + l4_offset);
+
+		if (iph->saddr < iph->daddr) {
+			lower_port = udph->source;
+			upper_port = udph->dest;
+		} else {
+			lower_port = udph->dest;
+			upper_port = udph->source;
+		}
+	} else {
+		// non tcp/udp protocols
+		lower_port = 0;
+		upper_port = 0;
+	}
+
+	flow.protocol = iph->protocol;
+	flow.lower_ip = lower_ip;
+	flow.upper_ip = upper_ip;
+	flow.lower_port = lower_port;
+	flow.upper_port = upper_port;
+
+	idx = (lower_ip + upper_ip + iph->protocol + lower_port + upper_port) % NUM_ROOTS;
+	ret = ndpi_tfind(&flow, (void*)&ndpi_flows_root[idx], node_cmp);
+
+	if (ret == NULL) {
+	  
+		if (ndpi_flow_count == MAX_NDPI_FLOWS) {
+			printf("\nERROR: maximum number of flows supported by this demo application (%u) has been exceeded! New flows from this source (PCAP file or interface) will be discarded!\n", MAX_NDPI_FLOWS);
+			return NULL;
+		}
+
+		struct ndpi_flow *newflow = (struct ndpi_flow*) malloc(sizeof(struct ndpi_flow));
+
+		if (newflow == NULL) {
+			printf("[NDPI] %s(1): not enough memory\n", __FUNCTION__);
+			return NULL;
+		}
+
+		memset(newflow, 0, sizeof(struct ndpi_flow));
+		newflow->protocol = iph->protocol;
+		newflow->lower_ip = lower_ip, newflow->upper_ip = upper_ip;
+		newflow->lower_port = lower_port, newflow->upper_port = upper_port;
+
+		inet_ntop(AF_INET, &lower_ip, newflow->lower_name, sizeof(newflow->lower_name));
+		inet_ntop(AF_INET, &upper_ip, newflow->upper_name, sizeof(newflow->upper_name));
+
+		if ((newflow->ndpi_flow_struct_pointer = create_ndpi_flow_struct_pointer()) == NULL) {
+			printf("[NDPI] %s(2): not enough memory\n", __FUNCTION__);
+			return NULL;
+		}
+
+		ndpi_tsearch(newflow, (void*)&ndpi_flows_root[idx], node_cmp); /* Add */
+
+		return newflow;
+
+	} else {
+		struct ndpi_flow *flow = *(struct ndpi_flow**) ret;
+		return flow;
+	}
+}
+
+void print_flow(char *pcap_file_name, struct ndpi_flow *flow) {
+  
+	char buffer[50];
+	
+	char result[1000];
+	strcpy(result, pcap_file_name);
+	
+	strcat(result, "  (");
+	sprintf(buffer, "%u", flow->processed_packets);
+	strcat(result, buffer);
+	strcat(result, " pckts) ");
+	
+	if (strlen(result) <=50) {
+	  for (int i = 0; i < (50 - strlen(result)); i++) {
+	    strcat(result, " ");
+	  }
+	}
+	
+	sprintf(buffer, "\t%s:%u <-> %s:%u ", flow->lower_name, ntohs(flow->lower_port), flow->upper_name, ntohs(flow->upper_port));
+	strcat(result, buffer);
+	
+	for (int i = 0; i < (50 - strlen(buffer)); i++) {
+	  strcat(result, " ");
+	}
+	
+	strcat(result, "proto: ");
+	strcat(result, ndpi_get_result_ip_name(ndpi_detection_module_struct_pointer, flow->ndpi_flow_struct_pointer));
+	
+	if ((ndpi_get_result_base_id(flow->ndpi_flow_struct_pointer) != NDPI_RESULT_BASE_STILL_UNKNOWN) && (ndpi_get_result_base_id(flow->ndpi_flow_struct_pointer) != NDPI_RESULT_BASE_UNKNOWN)) {
+	  strcat(result, "->");
+	  strcat(result, ndpi_get_result_base_name(ndpi_detection_module_struct_pointer, flow->ndpi_flow_struct_pointer));
+	}
+	
+	if ((ndpi_get_result_app_id(flow->ndpi_flow_struct_pointer) != NDPI_RESULT_APP_STILL_UNKNOWN) && (ndpi_get_result_app_id(flow->ndpi_flow_struct_pointer) != NDPI_RESULT_APP_UNKNOWN)) {
+	  strcat(result, "->");
+	  strcat(result, ndpi_get_result_app_name(ndpi_detection_module_struct_pointer, flow->ndpi_flow_struct_pointer));
+	}
+	
+	if ((ndpi_get_result_content_id(flow->ndpi_flow_struct_pointer) != NDPI_RESULT_CONTENT_STILL_UNKNOWN) && (ndpi_get_result_content_id(flow->ndpi_flow_struct_pointer) != NDPI_RESULT_CONTENT_UNKNOWN)) {
+	  strcat(result, ", content: ");
+	  strcat(result, ndpi_get_result_content_name(ndpi_detection_module_struct_pointer, flow->ndpi_flow_struct_pointer));
+	}
+	
+	if (((ndpi_get_result_service_id(flow->ndpi_flow_struct_pointer) != NDPI_RESULT_SERVICE_STILL_UNKNOWN) && (ndpi_get_result_service_id(flow->ndpi_flow_struct_pointer) != NDPI_RESULT_SERVICE_UNKNOWN)) || (strlen(ndpi_get_result_domain_service_name(flow->ndpi_flow_struct_pointer)) > 0)) {
+	  strcat(result, ", service: ");
+	  strcat(result, ndpi_get_result_service_name(ndpi_detection_module_struct_pointer, flow->ndpi_flow_struct_pointer));
+	  
+	  if (strlen(ndpi_get_result_domain_service_name(flow->ndpi_flow_struct_pointer)) > 0) {
+	    strcat(result, " [");
+	    strcat(result, ndpi_get_result_domain_service_name(flow->ndpi_flow_struct_pointer));
+	    strcat(result, "]");
+	  }
+	}
+	
+	if (((ndpi_get_result_cdn_id(flow->ndpi_flow_struct_pointer) != NDPI_RESULT_CDN_STILL_UNKNOWN) && (ndpi_get_result_cdn_id(flow->ndpi_flow_struct_pointer) != NDPI_RESULT_CDN_UNKNOWN)) || (strlen(ndpi_get_result_domain_cdn_name(flow->ndpi_flow_struct_pointer)) > 0)) {
+	  strcat(result, ", cdn: ");
+	  strcat(result, ndpi_get_result_cdn_name(ndpi_detection_module_struct_pointer, flow->ndpi_flow_struct_pointer));
+	  
+	  if (strlen(ndpi_get_result_domain_cdn_name(flow->ndpi_flow_struct_pointer)) > 0) {
+	    strcat(result, " [");
+	    strcat(result, ndpi_get_result_domain_cdn_name(flow->ndpi_flow_struct_pointer));
+	    strcat(result, "]");
+	  }
+	}
+	
+	printf("%s\n", result);
+}
+
+static void node_print_unknown_proto_walker(const void *node, ndpi_VISIT which, int depth, void *pcap_file_name) {
+  
+	struct ndpi_flow *flow = *(struct ndpi_flow**) node;
+
+	if (flow->detection_completed) {
+		return;
+	}
+	
+	print_flow((char *) pcap_file_name, flow);
 }
 
 void handle_pcap_file(char *pcap_file_name) {
-
-	clear_ndpi_flow_struct_pointer(ndpi_flow_struct_pointer);
 
 	char pcap_error_buffer[PCAP_ERRBUF_SIZE];
 	pcap_t *pcap_handle = pcap_open_offline(pcap_file_name, pcap_error_buffer);
@@ -62,94 +431,54 @@ void handle_pcap_file(char *pcap_file_name) {
 	struct pcap_pkthdr header;
 	const u_char *packet;
 	
-	unsigned long processed_packets_by_ndpi = 0;
-	unsigned long processed_packets = 0;
-	int flow_detected = 0;
-	
 	while (packet = pcap_next(pcap_handle, &header)) {
-		u_int64_t time = ((uint64_t) header.ts.tv_sec) * 1000000 + header.ts.tv_usec;
 		
-		if (!flow_detected) {
-		  flow_detected = process_packet_by_ndpi(time, header.len, (u_char *) packet);
-		  processed_packets_by_ndpi++;
+		u_int64_t time = ((uint64_t) header.ts.tv_sec) * 1000000 + header.ts.tv_usec;
+		const struct ndpi_ethhdr *ethernet = (struct ndpi_ethhdr *) packet;
+		u_int16_t ip_offset = sizeof(struct ndpi_ethhdr);
+		
+		if (ntohs(ethernet->h_proto) == 0x8100 /* VLAN */) {
+			ip_offset += 4;
 		}
 		
-		processed_packets++;
+		struct ndpi_iphdr *iph = (struct ndpi_iphdr *) &packet[ip_offset];
+		u_int16_t ipsize = header.len - ip_offset;
+		
+		struct ndpi_flow *flow = get_ndpi_flow(iph, ipsize, ntohs(iph->tot_len) - (iph->ihl * 4));
+		
+		if (flow == NULL) {
+		  continue;
+		}
+		
+		if (flow->detection_completed) {
+		  continue;
+		}
+		
+		flow->processed_packets++;
+		
+		flow->detection_completed = ndpi_process_ip_packet(ndpi_detection_module_struct_pointer, flow->ndpi_flow_struct_pointer, (uint8_t *) iph, ipsize, time);
+		
+		if (flow->detection_completed) {
+		  print_flow(pcap_file_name, flow);
+		  free_ndpi_flow(flow);
+		}
 	}
-	
-	char no_of_packets[15];
-	char result[1000];
-	strcpy(result, pcap_file_name);
-	
-	for (int i = 0; i < (35 - strlen(pcap_file_name)); i++) {
-	  strcat(result, " ");
-	}
-	
-	strcat(result, "proto: ");
-	strcat(result, ndpi_get_result_ip_name(ndpi_detection_module_struct_pointer, ndpi_flow_struct_pointer));
-	
-	if ((ndpi_get_result_base_id(ndpi_flow_struct_pointer) != NDPI_RESULT_BASE_STILL_UNKNOWN) && (ndpi_get_result_base_id(ndpi_flow_struct_pointer) != NDPI_RESULT_BASE_UNKNOWN)) {
-	  strcat(result, "->");
-	  strcat(result, ndpi_get_result_base_name(ndpi_detection_module_struct_pointer, ndpi_flow_struct_pointer));
-	}
-	
-	if ((ndpi_get_result_app_id(ndpi_flow_struct_pointer) != NDPI_RESULT_APP_STILL_UNKNOWN) && (ndpi_get_result_app_id(ndpi_flow_struct_pointer) != NDPI_RESULT_APP_UNKNOWN)) {
-	  strcat(result, "->");
-	  strcat(result, ndpi_get_result_app_name(ndpi_detection_module_struct_pointer, ndpi_flow_struct_pointer));
-	}
-	
-	if ((ndpi_get_result_content_id(ndpi_flow_struct_pointer) != NDPI_RESULT_CONTENT_STILL_UNKNOWN) && (ndpi_get_result_content_id(ndpi_flow_struct_pointer) != NDPI_RESULT_CONTENT_UNKNOWN)) {
-	  strcat(result, ", content: ");
-	  strcat(result, ndpi_get_result_content_name(ndpi_detection_module_struct_pointer, ndpi_flow_struct_pointer));
-	}
-	
-	if (((ndpi_get_result_service_id(ndpi_flow_struct_pointer) != NDPI_RESULT_SERVICE_STILL_UNKNOWN) && (ndpi_get_result_service_id(ndpi_flow_struct_pointer) != NDPI_RESULT_SERVICE_UNKNOWN)) || (strlen(ndpi_get_result_domain_service_name(ndpi_flow_struct_pointer)) > 0)) {
-	  strcat(result, ", service: ");
-	  strcat(result, ndpi_get_result_service_name(ndpi_detection_module_struct_pointer, ndpi_flow_struct_pointer));
-	  
-	  if (strlen(ndpi_get_result_domain_service_name(ndpi_flow_struct_pointer)) > 0) {
-	    strcat(result, " [");
-	    strcat(result, ndpi_get_result_domain_service_name(ndpi_flow_struct_pointer));
-	    strcat(result, "]");
-	  }
-	}
-	
-	if (((ndpi_get_result_cdn_id(ndpi_flow_struct_pointer) != NDPI_RESULT_CDN_STILL_UNKNOWN) && (ndpi_get_result_cdn_id(ndpi_flow_struct_pointer) != NDPI_RESULT_CDN_UNKNOWN)) || (strlen(ndpi_get_result_domain_cdn_name(ndpi_flow_struct_pointer)) > 0)) {
-	  strcat(result, ", cdn: ");
-	  strcat(result, ndpi_get_result_cdn_name(ndpi_detection_module_struct_pointer, ndpi_flow_struct_pointer));
-	  
-	  if (strlen(ndpi_get_result_domain_cdn_name(ndpi_flow_struct_pointer)) > 0) {
-	    strcat(result, " [");
-	    strcat(result, ndpi_get_result_domain_cdn_name(ndpi_flow_struct_pointer));
-	    strcat(result, "]");
-	  }
-	}
-	
-	int len = strlen(result);
-	
-	for (int i = 0; i < (150 - len); i++) {
-	  strcat(result, " ");
-	}
-	
-	strcat(result, "Inspected packets: ");
-	
-	sprintf(no_of_packets, "%lu", processed_packets_by_ndpi);
-	
-	strcat(result, no_of_packets);
-	strcat(result, "/");
-	
-	sprintf(no_of_packets, "%lu", processed_packets);
-	strcat(result, no_of_packets);
-	
-	printf("%s\n", result);
 	
 	pcap_close(pcap_handle);
+	
+	for (int i = 0; i < NUM_ROOTS; i++) {
+		ndpi_twalk(ndpi_flows_root[i], node_print_unknown_proto_walker, pcap_file_name);
+	}
+	
+	for (int i = 0; i < NUM_ROOTS; i++) {
+		ndpi_tdestroy(ndpi_flows_root[i], ndpi_flow_freer);
+		ndpi_flows_root[i] = NULL;
+	}
 }
 
 int main(int argc, char **argv) {
 
 	ndpi_detection_module_struct_pointer = create_ndpi_detection_module_struct_pointer(1000000, NULL);
-	ndpi_flow_struct_pointer = create_ndpi_flow_struct_pointer();
 
 	DIR *dir1, *dir2;
 	struct dirent *ent1, *ent2;
@@ -189,6 +518,5 @@ int main(int argc, char **argv) {
 		return EXIT_FAILURE;
 	}
 
-	delete_ndpi_flow_struct_pointer(ndpi_flow_struct_pointer);
 	delete_ndpi_detection_module_struct_pointer(ndpi_detection_module_struct_pointer);
 }
